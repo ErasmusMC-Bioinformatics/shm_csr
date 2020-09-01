25c78582c8dbc7eb2e9d3293642c901f54b225ef
davidvanzessen <d.vanzessen@erasmusmc.nl>
2020-09-01 17:19:42 +0200

    Update makedb.sh

--- change_o/makedb.sh
+++ change_o/makedb.sh
@@ -29,7 +29,7 @@ mkdir $PWD/outdir
 
 echo "makedb: $PWD/outdir"
 
-python3 $dir/MakeDb.py imgt -i $input --outdir $PWD/outdir --outname output $noparse $scores $regions
+MakeDb.py imgt -i $input --outdir $PWD/outdir --outname output $noparse $scores $regions
 
 mv $PWD/outdir/output_db-pass.tab $output
 
