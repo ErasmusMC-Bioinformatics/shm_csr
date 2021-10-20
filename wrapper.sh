6c28c63f5c97c440c742029c94328d27ecaa4a6f
emma <emma.sehlhoff@hotmail.nl>
2021-10-20 10:36:16 +0200

    fixed; Revalued remove_files.txt and removed files that do not must be removed. Changed the removing code and added vlags.

--- wrapper.sh
+++ wrapper.sh
@@ -892,27 +892,20 @@ echo "</center></html>" >> $log
 
 IFS="$tIFS"
 
-rm -r results/baseline
-rm -r files
-xargs rm < remove_files.txt
+echo "---------------- remove_files----------------"
+echo "---------------- remove_files----------------<br />" >> $log
+
+rm -r -v results/baseline
+rm -r -v files
+filename='remove_files.txt'
+
+while read file; do
+    rm -v $file
+done < "$filename"
+
 
 echo "---------------- Done! ----------------"
 echo "---------------- Done! ----------------<br />" >> $outdir/log.html
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
