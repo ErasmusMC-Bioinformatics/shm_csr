44950c8f4cca9c1b3b9d4ee9326d2a43659064ac
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2023-01-31 12:40:50 +0100

    Simper timer script

--- show_time_as_float.c
+++ show_time_as_float.c
@@ -1,25 +1,16 @@
 /* script adapted from https://www.nu42.com/2021/07/windows-c-time-in-nanoseconds.html */
-#include <inttypes.h>
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
-static double
-time_now(void)
+int main(void)
 {
     struct timespec ts;
 
     if (timespec_get(&ts, TIME_UTC) != TIME_UTC)
     {
         fputs("timespec_get failed!", stderr);
-        return 0;
+        return 1;
     }
-    return (double)ts.tv_sec + ((double)ts.tv_nsec / (double)1000000000);
-}
-
-int main(void)
-{
-    printf("%lf\n", time_now());
-    return EXIT_SUCCESS;
+    printf("%d.%d\n", ts.tv_sec, ts.tv_nsec);
+    return 0;
 }
